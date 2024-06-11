9376d2ddcb8c87a0ce9c1338e17656c1e7d982dd
carldomond7 <124755395+carldomond7@users.noreply.github.com>
2024-06-11 12:50:05 -0700

    Update main.py

--- Project/app/main.py
+++ Project/app/main.py
@@ -38,7 +38,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://gtmhtml.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
